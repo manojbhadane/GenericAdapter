15edd93341dc41ee8fc9652a1114a75be4a132d5
manoj.bhadane <manojbhadane777@gmail.com>
2019-05-29 14:54:04 +0530

    small changes

--- app/src/main/java/com/manojbhadane/genericadaptersample/adapter/SearchActivity.java
+++ app/src/main/java/com/manojbhadane/genericadaptersample/adapter/SearchActivity.java
@@ -1,28 +1,29 @@
 package com.manojbhadane.genericadaptersample.adapter;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.manojbhadane.genericadapter.GenericAdapter;
+import com.manojbhadane.genericadapter.GenericFilterAdapter;
 import com.manojbhadane.genericadaptersample.PeopleModel;
 import com.manojbhadane.genericadaptersample.R;
-import com.manojbhadane.genericadaptersample.databinding.ActivityMainBinding;
+import com.manojbhadane.genericadaptersample.databinding.ActivitySearchBinding;
 import com.manojbhadane.genericadaptersample.databinding.ListitemMainBinding;
 
 import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
-    private ActivityMainBinding mDataBinding;
+    private ActivitySearchBinding mDataBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_search);
 
         // List
         ArrayList<PeopleModel> arrayList = new ArrayList<>();
@@ -36,7 +37,7 @@ public class SearchActivity extends AppCompatActivity {
         mDataBinding.recylerview.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
 
         // List Adapter
-        mDataBinding.recylerview.setAdapter(new GenericAdapter<PeopleModel, ListitemMainBinding>(this, arrayList) {
+        mDataBinding.recylerview.setAdapter(new GenericFilterAdapter<PeopleModel, ListitemMainBinding>(this, arrayList) {
             @Override
             public int getLayoutResId() {
                 return R.layout.listitem_main;
@@ -51,6 +52,24 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onItemClick(PeopleModel model, int position) {
 
+            }
+
+            @Override
+            public View getSearchField() {
+                return mDataBinding.edtSearch;
+            }
+
+            @Override
+            public ArrayList<PeopleModel> performFilter(String searchText, ArrayList<PeopleModel> originalList) {
+                ArrayList<PeopleModel> filteredList = new ArrayList<>();
+                for (PeopleModel row : originalList) {
+                    if (row.getName().toLowerCase().contains(searchText.toLowerCase()) || row.getName().contains(searchText) ||
+                            row.getAddress().toLowerCase().contains(searchText.toLowerCase())) {
+                        filteredList.add(row);
+                    }
+                }
+
+                return filteredList;
             }
         });
     }
